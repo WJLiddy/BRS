37ede8aec80a0c156326eaef7ce0bdb29362e4ab
WJLIDDY <wjl36@case.edu>
2016-10-08 01:40:55 -0400

    Adding more UDP fixes, endianness fix

--- Bob Ross Sim/Assets/UDPReceive.cs
+++ Bob Ross Sim/Assets/UDPReceive.cs
@@ -39,7 +39,7 @@ public class UDPReceive : MonoBehaviour
         GUIStyle style = new GUIStyle();
         style.alignment = TextAnchor.UpperLeft;
         GUI.Box(rectObj, "On port: " + port + " #\n"
-                + "ID 0 X Y Z: " + pos[0,0], " " + pos[0,1] + pos[0,2] + " \n");
+                + "ID 0 X Y Z: " + pos[0,0] + " " + pos[0,1] + " " + pos[0,2] + " \n");
     }
 
     // init
@@ -68,18 +68,45 @@ public class UDPReceive : MonoBehaviour
             {
                 // Bytes empfangen.
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
-                byte[] data = client.Receive(ref anyIP);
+                byte[] endian_data = client.Receive(ref anyIP);
+                byte[] data = new byte[endian_data.Length];
+
+                // Reverse int at the start
+                for (int i = 0; i != 4; i++)
+                {
+                    data[i] = endian_data[3 - i];
+                }
+
+                // fetch 6 double
+                for (int dbl = 0; dbl != 6; dbl++)
+                {
+                    for (int b = 0; b != 8; b++)
+                    {
+                        data[4 + (dbl*8) + b] = endian_data[4 + (dbl * 8) + (7-b)];
+                    }
+                }
+
+                int start = (4 + (6 * 8));
+                // fetch 3 int
+                for (int col = 0; col != 3; col++)
+                {
+                    for (int b = 0; b != 4; b++)
+                    {
+                        data[start + (col * 4) + b] = endian_data[start + (col * 4) + (3 - b)];
+                    }
+                }
 
                 // ID
                 int posindex = 4;
                 int id = BitConverter.ToInt32(data, 0);
-                for(int i = 0; i != 8; i++)
+
+                for(int i = 0; i != 6; i++)
                 {
                     //update position vector
                     pos[id,i] = BitConverter.ToDouble(data, posindex + (i*8));
                 }
 
-                int colindex = (4 + 64);
+                int colindex = (4 + (8*6));
 
                 for (int i = 0; i != 3; i++)
                 {
@@ -91,13 +118,5 @@ public class UDPReceive : MonoBehaviour
                 print(err.ToString());
             }
         }
-    }
-
-    // getLatestUDPPacket
-    // cleans up the rest
-    public string getLatestUDPPacket()
-    {
-        allReceivedUDPPackets = "";
-        return lastReceivedUDPPacket;
     }
 }
