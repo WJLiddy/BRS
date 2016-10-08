30d6d56d14ed734902238b2802396eeb1b95f4d1
WJLIDDY <wjl36@case.edu>
2016-10-07 23:48:30 -0400

    Unity GO!

---
+++ Bob Ross Sim/Assets/Plane.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plane : MonoBehaviour {
+
+    public MovieTexture movTexture;
+    void Start()
+    {
+        movTexture = ((MovieTexture)GetComponent<Renderer>().material.mainTexture);
+        movTexture.Play();
+    }
+}
+
