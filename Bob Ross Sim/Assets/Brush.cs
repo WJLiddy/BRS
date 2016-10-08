189ce7bcc6e904b09c2b18d5a2f9d034b0be9bec
WJLIDDY <wjl36@case.edu>
2016-10-08 15:47:44 -0400

    Color mixing models

--- Bob Ross Sim/Assets/Brush.cs
+++ Bob Ross Sim/Assets/Brush.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Brush : MonoBehaviour {
 
+    bool paint_start = false;
     // Use this for initialization
     void Start()
     {
@@ -11,16 +12,28 @@ public class Brush : MonoBehaviour {
 
     }
 
+    float old_x;
+    float old_y;
     // Update is called once per frame
     void Update()
     {
+
         Vector3 oldpos = transform.position;
         transform.position = new Vector3(oldpos.x + (0.1F*Input.GetAxis("Mouse X")), oldpos.y + (0.1F * Input.GetAxis("Mouse Y")), (Input.GetMouseButton(0) ? -7.0F : -7.5F) );
         GameObject canvas = GameObject.Find("Canvas");
         CanvasOps cs = canvas.GetComponent<CanvasOps>();
 
-     //   if (Input.GetMouseButton(0))
-      //     cs.applyBrush(transform.position.x - canvas.transform.position.x, transform.position.y -  canvas.transform.position.y);
+        float x_send = transform.position.x - canvas.transform.position.x;
+        float y_send = transform.position.y - canvas.transform.position.y;
 
-    }
+        float changeamt = Mathf.Abs(old_x - x_send) + Mathf.Abs(old_y - y_send);
+
+        if (Input.GetMouseButton(0) && paint_start && changeamt > 0.01)
+            //we need substantial movment 
+            cs.applyBrush(old_x,old_y,x_send, y_send, 10, 0, 1.0F, 0.0F, 0.5F);
+
+        paint_start = Input.GetMouseButton(0);
+        old_x = x_send;
+        old_y = y_send;
+     }
 }
--- Bob Ross Sim/Assets/Scripts/CanvasOps.cs
+++ Bob Ross Sim/Assets/Scripts/CanvasOps.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.IO;
+using System.Collections.Generic;
+
 public class CanvasOps : MonoBehaviour
 {
     Texture2D texture;
-    float[,,] cmy = new float[400, 300, 3];
-    float[,] wetness = new float[400, 300];
+    // cmds
+    float[,,] cmy = new float[1600, 1200, 3];
+    float[,] wetness = new float[1600, 1200];
+    LinkedList<int[]> new_paint;
     System.Random r;
 
     void Start()
     {
+        new_paint = new LinkedList<int[]>();
         texture = LoadPNG(Application.dataPath + "/misc_materials/base.png");
         //convert texture to cmy completely dry.
         for (int x = 0; x != texture.width; x++)
@@ -23,6 +28,7 @@ public class CanvasOps : MonoBehaviour
                 cmy[x, y, 1] = ma;
                 cmy[x, y, 2] = ye;
                 wetness[x, y] = 0;
+                new_paint.AddFirst(new int[2] { x, y});
             }
         }
         GetComponent<Renderer>().material.mainTexture = texture;
@@ -33,16 +39,16 @@ public class CanvasOps : MonoBehaviour
     {
         //convert cmy to rgb
         //apply paint to these pixels
-        for (int x = 0; x != texture.width; x++)
+        foreach (int[] element in new_paint)
         {
-            for (int y = 0; y != texture.height; y++)
-            {
-                float r = (1f - cmy[x, y ,0]);
-                float g = (1f - cmy[x, y, 1]);
-                float b = (1f - cmy[x, y, 2]);
-                texture.SetPixel(x,y, new Color(r, g, b, 0));
-            }
+            int x = element[0];
+            int y = element[1];
+            float r = (1f - cmy[x, y ,0]);
+            float g = (1f - cmy[x, y, 1]);
+            float b = (1f - cmy[x, y, 2]);
+            texture.SetPixel(x,y, new Color(r, g, b, 0));
         }
+        new_paint = new LinkedList<int[]>();
         texture.Apply();
     }
 
@@ -61,28 +67,46 @@ public class CanvasOps : MonoBehaviour
         return tex;
     }
 
-    public void applyBrush(float x, float y, int brush_size, float cy, float ma, float ye, float str)
+    public void applyBrush(float old_x, float old_y, float targ_x, float targ_y, int brush_radius, float cy, float ma, float ye, float str)
     {
-        /**
-        // Transform on Y axis
-        y = -y;
-        //change x to reference top right corner
-        x = x + 1.5F;
-        y = y + 2.0F;
-        //Debug.Log(x + "," + y);
-        //scale x, y from 0.3 to 0.4: MULTIPLY BY 100
-        if (x < 0.0 || y < 0.0 || x >= 3 || y >= 4)
-            return;
+        //Don't draw over !
 
-        //apply paint to these pixels
-        for (int dx = -bra; dx != 4; dx++)
-        {
-            for (int dy = -3; dy != 4; dy++)
+        int delta_steps = 10;
+        for (int s = 0; s != delta_steps; s++)
+        { 
+           float x = old_x + ((targ_x - old_x) * ((float)s / delta_steps));
+           float y = old_y + ((targ_y - old_y) * ((float)s / delta_steps));
+            
+            // Transform on Y axis
+            y = -y;
+            //change x to reference top right corner
+            x = x + 1.5F;
+            y = y + 2.0F;
+            //scale x, y from 0.3 to 0.4: MULTIPLY BY 100
+            if (x < 0.0 || y < 0.0 || x >= 3 || y >= 4)
+                return;
+
+            //apply paint to these pixels
+            for (int dx = -brush_radius; dx != brush_radius; dx++)
             {
-                texture.SetPixel(400 - (dx + (int)(y * 100F)), 300 - (dy + (int)(x * 100F)), new Color(1F, 0, 0, 0));
+                for (int dy = -brush_radius; dy != brush_radius; dy++)
+                {
+                    if ((((float)dx * (float)dx) + ((float)dy * (float)dy)) < ((float)brush_radius * (float)brush_radius))
+                    {
+                        //don't ask
+                        int canvas_x = texture.width - (dx + (int)(y * 400F));
+                        int canvas_y = texture.height - (dy + (int)(x * 400F));
+                        if (canvas_x < 0 || canvas_y < 0 || canvas_x >= texture.width || canvas_y >= texture.height)
+                            continue;
+                        float new_paint_ratio = (str) / (1F + wetness[canvas_x, canvas_y]);
+                        cmy[canvas_x, canvas_y, 0] = (new_paint_ratio * cy) + ((1F - new_paint_ratio)*cmy[canvas_x, canvas_y, 0]);
+                        cmy[canvas_x, canvas_y, 1] = (new_paint_ratio * ma) + ((1F - new_paint_ratio)*cmy[canvas_x, canvas_y, 1]);
+                        cmy[canvas_x, canvas_y, 2] = (new_paint_ratio * ye) + ((1F - new_paint_ratio)*cmy[canvas_x, canvas_y, 2]);
+                        wetness[canvas_x, canvas_y] = str;
+                        new_paint.AddFirst(new int[2] { canvas_x, canvas_y });
+                    }
+                }
             }
         }
-
-    */
     }
 }
