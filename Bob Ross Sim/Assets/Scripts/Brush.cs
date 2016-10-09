6215fd142830d23f2609c30b74a52afb86457b3a
WJLIDDY <wjl36@case.edu>
2016-10-09 03:28:17 -0400

    new

--- Bob Ross Sim/Assets/Scripts/Brush.cs
+++ Bob Ross Sim/Assets/Scripts/Brush.cs
@@ -55,7 +55,7 @@ public class Brush : MonoBehaviour {
         Vector3 oldpos = transform.position;
         //mouse
         //transform.position = new Vector3(oldpos.x + (0.1F*Input.GetAxis("Mouse X")), oldpos.y + (0.1F * Input.GetAxis("Mouse Y")), (Input.GetMouseButton(0) ? -6.4F : -6.7F) );
-        transform.position = new Vector3(orig_x + -(1.5f * x), orig_y + (1.5f * y), (is_painting == 1) ? -6.4F : -6.7F);
+        transform.position = new Vector3(orig_x + -(1.5f * x), orig_y + (1.5f * y), (is_painting == 1) ? -6.2F : -6.5F);
 
 
         GameObject canvas = GameObject.Find("Canvas"+brush_id);
@@ -71,28 +71,55 @@ public class Brush : MonoBehaviour {
          * 
          * */
 
+        // TRY TO DEBUG
+
+
+
         int c_targ = 0;
         // Ok, here , we look up drawing on other people's images
         if (transform.position.x > 1.5 && transform.position.y < 5)
-            c_targ = 4;
-        else if (transform.position.x < 1.5 && transform.position.y < 5)
+        {
             c_targ = 3;
-        else if (transform.position.x > 1.5 && transform.position.y > 5)
-            c_targ = 1;
-        else if (transform.position.x < 1.5 && transform.position.y > 5)
-            c_targ = 2;
 
+        }
+        else if (transform.position.x < 1.5 && transform.position.y < 5)
+        {
+            c_targ = 2;
+        }
+        else if (transform.position.x > 1.5 && transform.position.y > 5)
+        {
+            c_targ = 1;
+        }
+        else if (transform.position.x < 1.5 && transform.position.y > 5)
+        {
+            c_targ = 0;
+        }
+        
         // Send X from 0..1
         // Send Y from 0.. 1
         float changeamt = Mathf.Abs(old_x - x_send) + Mathf.Abs(old_y - y_send);
 
-        if (c_targ != brush_id && ((is_painting == 1) && paint_start && changeamt > 0.001))
+       
+
+        //??? CHANGEAMY DOESNT WORK HERE
+        if ((c_targ != brush_id) && (is_painting == 1) && (paint_start))
         {
+            Debug.Log("EXECUTE");
             // WE ARE BEING eVIL!
             evil++;
-            GameObject.Find("Canvas" + c_targ).GetComponent<CanvasOps>().applyBrush(old_x, old_y, x_send, y_send, 15, cy, ye, ma, 0.2F);
 
-        } else if ((is_painting == 1) && paint_start && changeamt > 0.001)
+            // NEW POS UPDATE
+            // UGLY but it'll go.
+            Vector3 d = new Vector3(newX(c_targ) + -(1.5f * x), newY(c_targ) + (1.5f * y), (is_painting == 1) ? -6.2F : -6.5F);
+
+            float x_send_e = ((d.x + -GameObject.Find("Canvas" + c_targ).GetComponent<CanvasOps>().transform.position.x + 1F) / 2F);
+            float y_send_e = -((d.y + -GameObject.Find("Canvas" + c_targ).GetComponent<CanvasOps>().transform.position.y + 1F) / 2F) + 1;
+
+
+
+            GameObject.Find("Canvas" + c_targ).GetComponent<CanvasOps>().applyBrush(x_send_e, y_send_e, x_send_e+0.1f, y_send_e+0.1f, 15, cy, ye, ma, 0.2F);
+
+        } else if ((is_painting == 1) && paint_start && changeamt > 0.002)
         {
             //we are writing on our own, so add passion
             passion++;
@@ -103,4 +130,42 @@ public class Brush : MonoBehaviour {
         old_x = x_send;
         old_y = y_send;
      }
+
+    float newX(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                return 0.5f;
+            case 1:
+                return 2.5f;
+            case 2:
+                return 0.5f;
+                break;
+            case 3:
+                return 2.5f;
+        }
+        return -1f;
+    }
+
+    float newY(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                return 6f;
+                break;
+            case 1:
+                return 6f;
+                break;
+            case 2:
+                return 4f;
+                break;
+            case 3:
+                return 4f;
+                break;
+        }
+        return -1;
+
+      }
 }
